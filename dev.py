fe3fcb1c4f64671dc909bb3ada1ce788d2975f0b
Rebecca Turner <rbt@sent.as>
2020-07-10 20:44:13 -0400

    dev

--- dev.py
+++ dev.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3.8
 import asyncio
 from typing import List, Optional
+import webbrowser
+from os import path
 
 
 async def run(program: str, *args: str):
@@ -9,11 +11,18 @@ async def run(program: str, *args: str):
 
 
 async def sass_watch(watch: str, render: str):
+    print("Compiling Sass files in", watch, "to", render)
     await run("sass", "--watch", f"{watch}:{render}")
 
 
 async def serve_static(root: str = "."):
+    print("Serving static files in ", path.realpath(root))
     await run("sfz", "--render-index", "--bind", "127.0.0.1", root)
+
+
+async def open_browser(address: str):
+    print("Opening", address, "in your browser")
+    webbrowser.open_new_tab(address)
 
 
 async def amain():
@@ -21,6 +30,7 @@ async def amain():
         serve_static("."),
         sass_watch("plugin/highlight/rustconf.sass", "plugin/highlight/rustconf.css"),
         sass_watch("css/theme/source", "dist/theme"),
+        open_browser("http://localhost:5000/"),
     )
 
 
